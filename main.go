2cab5b747902de229897c8a30aabd0dca8d5d784
minsan <leexikang@gmail.com>
2018-09-15 17:13:25 +0630

    deploy to heroku

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"image/png"
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/boombuler/barcode"
@@ -9,7 +18,15 @@ import (
 )
 
 func main() {
+
 	// Create the barcode
+	fmt.Println("Starting the application...")
+	ciphertext := encrypt([]byte("Hello World"), "password")
+	fmt.Printf("Encrypted: %x\n", ciphertext)
+
+	plaintext := decrypt(ciphertext, "password")
+	fmt.Printf("Decrypted: %s\n", plaintext)
+
 	qrCode, _ := qr.Encode("Hello World", qr.M, qr.Auto)
 
 	// Scale the barcode to 200x200 pixels
@@ -21,4 +38,64 @@ func main() {
 
 	// encode the barcode as png
 	png.Encode(file, qrCode)
+	http.HandleFunc("/", hello)
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
+}
+
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
 }
